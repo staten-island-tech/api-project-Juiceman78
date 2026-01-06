3922963436a425672174717ad85fbf332fd0d679
Juiceman78 <vincentz78@nycstudents.net>
2026-01-06 14:01:25 -0500

    Reddit complainer. THis guy is not a genius he doesn't know shit. THe kid was too cocky to know and he told him to go on reddit. Mr whalen reddit alt account.

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -1,18 +1,35 @@
+// Get the container where Digimon cards will go
+const container = document.getElementById("app");
+// Fetch Digimon data from API
 async function getData() {
   try {
-    // get data from API
-    const response = await fetch(`https://digimon-api.vercel.app/api/digimon`);
+    const response = await fetch("https://digimon-api.vercel.app/api/digimon");
+
     if (response.status != 200) {
       throw new Error(response);
-    } else {
-      //converts reponse into json we can use
-      const data = await response.json();
-      console.log(data);
-      document.getElementById("api-response").textContent = data.name;
     }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 
-getData();
+function inject(digimon) {
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <div class="card">
+      <div class="text">${digimon.name}</div>
+      <div class="text">${digimon.level}</div>
+      <img class="img" src="${digimon.img}">
+    </div>
+    `
+  );
+}
+
+const data = await getData();
+if (data) {
+  data.forEach((digimon) => inject(digimon));
+}
